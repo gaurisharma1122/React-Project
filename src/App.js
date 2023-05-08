151fb171e65acb747658b9d8767890ffcfd77c5e
gaurisharma1122 <gaurisharma1122@gmail.com>
2023-05-08 10:43:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import BarAdmissions from "./components/BarAdmissions";
+import Details from "./components/Details";
+import Education from "./components/Education";
+import Header from "./components/Header";
+import Languages from "./components/Languages";
+import NotableCases from "./components/NotableCases";
+import ProfessionalEndorsements from "./components/ProfessionalEndorsements";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header/>
+      <div className="w-[90%] md:w-[80%] mx-auto my-0">
+        <Details/>
+        <BarAdmissions/>
+        <Languages/>
+        <Education/>
+        <ProfessionalEndorsements/>
+        <NotableCases />
+      </div>
+
     </div>
   );
 }
